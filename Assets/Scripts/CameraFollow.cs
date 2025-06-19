e5392ecf195991613f6c0766d75768b012e35065
Truongvvv <167153590+Truongvvv@users.noreply.github.com>
2025-06-19 20:17:19 +0700

    8-19-6

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,22 +1,9 @@
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
-//{
-//    public Transform target; // Gán Player (Role_T)
-//    public Vector3 offset = new Vector3(1.5f, 2.5f, -4f);
-//    public float followSpeed = 10f;
-
-//    void LateUpdate()
-//    {
-//        if (target == null) return;
-
-//        Vector3 desiredPos = target.position + offset;
-//        transform.position = Vector3.Lerp(transform.position, desiredPos, followSpeed * Time.deltaTime);
-//    }
-//}
 {
-    public Transform target;  // Gán là Player (Role_T)
-    public Vector3 offset = Vector3.zero; // Bám sát
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
 
     void LateUpdate()
     {
